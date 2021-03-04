4aa23162179167106961aa5608562f6a047842e9
liucangsong <liucangsong@foxmail.com>
2021-03-04 11:07:06 +0800

    更新页面，将JQuery Ajax 替换为 axios

--- setup-maven.bat
+++
@@ -1,6 +0,0 @@
-c:
-cd %USERPROFILE%
-mkdir .m2
-cd .m2
-bitsadmin /transfer myDownLoadJob /download /priority normal "http://doc.canglaoshi.org/config/settings.xml" "%cd%/settings.xml"
-echo Done!
--- setup-maven.sh
+++
@@ -1,5 +0,0 @@
-cd
-mkdir .m2
-cd .m2
-wget http://doc.canglaoshi.org/config/settings.xml
-echo Done
